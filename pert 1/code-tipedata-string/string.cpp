6b08619a65a515798bca6ee0170bbbfddacbc04d
root <guidoboginbogin@gmail.com>
2025-04-23 14:29:48 +0700

    menambahkan catatan pertemuan

---
+++ pert 1/code-tipedata-string/string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+    string welcome = "hello world";
+    cout<< welcome;
+
+    return 0;
+}
+
---
+++ string_ipynb/string.py
@@ -0,0 +1,5 @@
+name = "hello world";
+print(name)
+
+barang = {"sepatu", "sendal", "baju", "celana", "kemeja"};
+print(barang(2))
