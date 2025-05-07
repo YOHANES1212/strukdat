3700a9878c0deb2fa8453bce5fcc8106d4694421
root <guidoboginbogin@gmail.com>
2025-05-07 16:57:59 +0700

    menambhakan latihan.js

---
+++ latihan_logic/latihan.js
@@ -0,0 +1,13 @@
+console.log("yohanes guido bogin")
+console.log("20240801090")
+
+let nama = "yohanes guido bogin"
+let angka = 20240801090;
+let baru = angka * 2
+console.log(nama)
+console.log(angka)
+console.log(baru)
+
+let angka2 = [20240801090];
+let angka_new = angka2.map((num) => num * 2);
+console.log(angka_new)               
